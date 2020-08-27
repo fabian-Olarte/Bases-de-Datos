f1d0667abfad6ae47e284aff9a328cbebb2e93e7
BatsySlayer <sa.fernandez@javeriana.edu.co>
2020-08-26 21:28:21 -0500

    Update taller2OlarteFernandezRoseroVasquez.sql

--- Talleres/taller2OlarteFernandezRoseroVasquez.sql
+++ Talleres/taller2OlarteFernandezRoseroVasquez.sql
@@ -74,22 +74,59 @@ insert into DetalleFactura Values ('F4',3,2,10000);
 
 --1. Cual es la mujer y el hombre (Nombres y Apellidos en un campo llamado Nombre Completo) con la factura de más valor (En pesos), siempre y cuando la factura haya sido despachada. Mostrar Nombre Completo y valor de la factura.
 
-/*with valorTotalXFactura(codigoFactura, valorTotal) as
-    (
-    select codigoFactura, codigoProducto
+with valorTotalXFactura(codigoFactura, valorTotal) as
+    (select detallefactura.codigoFactura, sum(DetalleFactura.cantidad * Producto.valorUnitario) as valorTotal
     from DetalleFactura, Factura, Producto
-    group by codigoFactura),*/
-select (INITCAP(Persona.nombre) || " " || INITCAP(Persona.apellido)) as NombreCompleto
-from Persona, Factura
-where Factura.fechaEntrega = null and Persona.tipoDocumento = Factura.tipoDocumento and Persona.documento = Factura.documento; 
+    where detallefactura.codigofactura = factura.codigofactura and detallefactura.codigoproducto = producto.codigoproducto
+    group by detallefactura.codigofactura)
+select distinct(Persona.nombre || ' ' || Persona.apellido) AS "Nombre Completo", valorTotal
+from Persona, valorTotalXFactura, Factura
+where persona.genero = 'M' and persona.documento = factura.documento and persona.tipodocumento = factura.tipodocumento and valortotalxfactura.codigofactura = factura.codigofactura and factura.fechaEntrega = some (select factura.fechaEntrega from factura where factura.fechaEntrega is not null)
+UNION
+select distinct(Persona.nombre || ' ' || Persona.apellido) AS "Nombre Completo", valorTotal
+from Persona, valorTotalXFactura, Factura
+where persona.genero = 'F' and persona.documento = factura.documento and persona.tipodocumento = factura.tipodocumento and valortotalxfactura.codigofactura = factura.codigofactura and factura.fechaEntrega = some (select factura.fechaEntrega from factura where factura.fechaEntrega is not null);
+
 
 --2. Cual es la factura (Codigo Factura, fecha de factura, persona(nombres y apellidos) ), con la mayor cantidad de artículos vendidos.Mostrar (Codigo Factura, fecha de factura, persona(nombres y apellidos), cantidad de artículos )
+with sumaCantidades(documento, suma) as
+    (select persona.documento, sum(detallefactura.cantidad)
+    from Persona, detallefactura, factura
+    where factura.documento = persona.documento and detallefactura.codigofactura = factura.codigofactura
+    group by persona.documento)
+select distinct Factura.codigoFactura, Factura.fechaFactura, (Persona.nombre || ' ' || Persona.apellido) AS "Nombre Completo", sumaCantidades.suma
+from factura, persona, detallefactura, sumaCantidades
+where factura.documento = persona.documento and detallefactura.codigofactura = factura.codigofactura and sumacantidades.documento = persona.documento and detallefactura.cantidad = (select max (detallefactura.cantidad)
+                                                                                                                                    from detallefactura);
+                
 
-select Factura.codigoFactura, Factura.fechaFactura, (Persona.nombre || ' ' || Persona.apellido) AS "Nombre Completo", DetalleFactura.cantidad
-from 
-    (select max (DetalleFactura.cantidad) as valorMaximo
-    from DetalleFactura
-    where Factura.codigoFactura = DetalleFactura.codigoFactura)
-where sum(DetalleFactura.cantidad) = valorMaximo;
+--3. Que cliente (Nombres y Apellidos)  ha comprado todos los productos (No necesariamente en la misma factura) .
+
+select DISTINCT (Persona.nombre || ' ' || Persona.apellido) AS "Nombre Completo", detallefactura.codigofactura
+from producto, factura, detallefactura, persona
+where detallefactura.codigofactura = factura.codigofactura and factura.documento = persona.documento and detallefactura.codigoproducto = producto.codigoproducto and persona.tipodocumento = factura.tipodocumento
+group by persona.nombre, persona.apellido, detallefactura.codigofactura
+having count(distinct detallefactura.codigoproducto) = count (producto.codigoproducto);
+
+--4. Mostrar un listado de la forma (Factura, Valor Factura, Numero de articulos diferentes, Persona, Fecha factura).
+
+with cant_productos(cod, valorfactura, NumeroArticulosdistintos)as(select DetalleFactura.CodigoFactura, sum(DetalleFactura.cantidad*Producto.valorUnitario) as ValorFactura, count(distinct detalleFactura.codigoproducto) as NumeroArticulosdistintos
+from Detallefactura, producto, factura
+where factura.codigoFactura = detalleFactura.codigoFactura and Detallefactura.codigoProducto = producto.codigoProducto 
+group by DetalleFactura.CodigoFactura)
+select distinct detallefactura.CodigoFactura, cant_productos.valorfactura,  cant_productos.NumeroArticulosdistintos, (INITCAP(Persona.nombre) || ' ' || INITCAP(Persona.apellido)) as NombreCompleto, factura.fechafactura
+from Detallefactura, producto, factura, cant_productos, persona
+where  factura.codigoFactura = detalleFactura.codigoFactura and Detallefactura.codigoProducto = producto.codigoProducto and detallefactura.codigoFactura=cant_productos.cod and persona.documento = factura.documento and persona.tipodocumento = factura.tipodocumento and factura.codigofactura = cant_productos.cod;
+
+--5. Cual es la factura en la que se tuvo el mayor retraso en la entrega, mostrar Codigo de la factura, cantidad de productos, y fecha factura, fecha de entrega, Dias de demora en entrega (Los días de demora en entrega se definen como los días transcurridos entre la fecha de entrega y la fecha de factura).
+
+with cant_productos(cod, suma)as(select detalleFactura.codigoFactura, sum(detalleFactura.cantidad)
+                                            from detalleFactura, Persona, Factura
+                                            where detalleFactura.codigofactura=Factura.codigofactura and Persona.documento=Factura.documento and Persona.tipoDocumento= Factura.tipoDocumento
+                                            group by detalleFactura.codigoFactura)
+select t.codigoFactura, cant_productos.suma as "Cantidad Productos" ,t.fechafactura as "Fecha factura", t.fechaentrega as "Fecha entrega",  t.fechaentrega-t.fechafactura AS "Dias de demora"
+from Persona, cant_productos, factura t
+where Persona.documento=t.documento and Persona.tipoDocumento= t.tipoDocumento and t.codigoFactura=cant_productos.cod and t.fechaentrega-t.fechafactura=(select max(factura.fechaentrega-factura.fechafactura) 
+                                                                                                                                                 from Factura) ;
 
 commit;
